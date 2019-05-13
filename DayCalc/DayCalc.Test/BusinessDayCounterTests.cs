1ed3201da66062b2fb7194299fc74f802e3e8e23
Daniel Fell <daniel.fell@hotelscombined.com>
2019-05-13 10:23:06 +1000

    Refactoring of logic, removing duplicated tests

--- DayCalc/DayCalc.Test/BusinessDayCounterTests.cs
+++ DayCalc/DayCalc.Test/BusinessDayCounterTests.cs
@@ -13,16 +13,24 @@ namespace DayCalc.Test
             new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1},
             new object[] { new DateTime(2013, 10, 5), new DateTime(2013, 10, 14), 5},
             new object[] { new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 61}
-            
+
         };
 
         public static object[][] BusinessDayTestData = new object[][] {
-                new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), new object[] { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) }, 1 },
-                new object[] { new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), new object[] { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) }, 0},
-                new object[] { new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), new object[] { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) }, 59}
+                new object[] { new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1 },
+                new object[] { new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), 0},
+                new object[] { new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 59}
 
             };
 
+        /// <summary>
+        /// Couldn't figure out a better way of passing them to the test as nested datetimes in the above
+        /// object [][] structures aren't accepted.
+        /// </summary>
+        public static List<DateTime> PublicHolidays => new List<DateTime>{
+            new DateTime(2013, 12, 25),
+            new DateTime(2013, 12, 26),
+            new DateTime(2014, 1, 1) };
 
         [Theory, MemberData(nameof(WeekdayTestData))]
         public void ShouldCalculateWeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, int result)
@@ -35,9 +43,10 @@ namespace DayCalc.Test
 
         
         [Theory, MemberData(nameof(BusinessDayTestData))]
-        public void ShouldCalculateBusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, List<DateTime> publicHolidays, int result)
+        public void ShouldCalculateBusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, int result)
         {
-            var actual = BusinessDayCounter.BusinessDaysBetweenTwoDates(firstDate, secondDate, publicHolidays);
+            
+            var actual = BusinessDayCounter.BusinessDaysBetweenTwoDates(firstDate, secondDate, PublicHolidays);
 
             actual.ShouldBe(result);
 
@@ -49,51 +58,67 @@ namespace DayCalc.Test
     {
         public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            if (secondDate <= firstDate)
+            if (IsValidDateRange(firstDate, secondDate))
             {
                 return 0;
             }
 
-            var dates = GenerateDateList(firstDate, secondDate);
-            var datesWithoutFirstAndLast = RemoveFirstAndLastDates(dates); // Could be a flag to be inclusive vs exclusive of start/end dates
-            var datesWithoutFandLandWeekends = RemoveWeekendDays(datesWithoutFirstAndLast);
-            int[] intArray = new[] { 1,1,3,4,5};
+            var dateList = GenerateWeekdayDateList(firstDate, secondDate);
 
-            intArray.Where(i => i % 2 == 0).Sum();
+            return dateList.Count();
+        }
 
-            return datesWithoutFandLandWeekends.Count();
+        private static bool IsValidDateRange(DateTime firstDate, DateTime secondDate)
+        {
+            return firstDate >= secondDate;
         }
 
         public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate,
             IList<DateTime> publicHolidays)
         {
-            return 0;
+            if (IsValidDateRange(firstDate, secondDate))
+            {
+                return 0;
+            }
+
+            var dateList = GenerateWeekdayDateList(firstDate, secondDate);
+            dateList.RemoveAll(publicHolidays.Contains);
+
+            return dateList.Count();
+        }
+
+
+        private static List<DateTime> GenerateWeekdayDateList(DateTime firstDate, DateTime secondDate)
+        {
+            var dateList = GenerateDateList(firstDate, secondDate);
+            dateList = RemoveFirstAndLastDates(dateList); // Could accept a flag to be inclusive vs exclusive of start/end dateList
+            dateList = RemoveWeekendDays(dateList);
+            return dateList;
         }
 
         private static List<DateTime> GenerateDateList(DateTime firstDate, DateTime secondDate)
-        {
-
-            // build a collection of dates and go from there...
-            var dates = new List<DateTime>();
+        { 
+            // build a List of dateList to manipulate and go from there...
+            var dateList = new List<DateTime>();
             var dateToAdd = firstDate;
 
             while (dateToAdd <= secondDate)
             {
-                dates.Add(dateToAdd);
+                dateList.Add(dateToAdd);
                 dateToAdd = dateToAdd.AddDays(1);
             }
 
-            return dates;
+            return dateList;
         }
 
-        private static List<DateTime> RemoveWeekendDays(IEnumerable<DateTime> datesWithoutFirstAndLast)
+        private static List<DateTime> RemoveWeekendDays(IEnumerable<DateTime> dateList)
         {
-            return datesWithoutFirstAndLast.Where(d => d.DayOfWeek != DayOfWeek.Saturday && d.DayOfWeek != DayOfWeek.Sunday).ToList();
+            return dateList.Where(d => d.DayOfWeek != DayOfWeek.Saturday && d.DayOfWeek != DayOfWeek.Sunday).ToList();
         }
 
-        private static IEnumerable<DateTime> RemoveFirstAndLastDates(List<DateTime> dates)
+        private static List<DateTime> RemoveFirstAndLastDates(List<DateTime> dateList)
         {
-            return dates.Skip(1).Take(dates.Count() - 2);
+            return dateList.Skip(1).Take(dateList.Count() - 2).ToList();
         }
     }
 }
